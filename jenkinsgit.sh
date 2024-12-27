0608c9de0cfef3ccc99a4b905915d596bb131d3e
JuriyaniMohan <106726006+JuriyaniMohan@users.noreply.github.com>
2024-12-27 23:45:01 +0530

    Update jenkinsgit.sh

--- jenkinsgit.sh
+++ jenkinsgit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "########### this wass run by Jenkins###############"
 echo " Giving System Info "
-systeminfo="$(date);$(uptime);$(df-Th)"
+systeminfo="$(date);$(uptime);$(uname -r)"
 echo $systeminfo >> /tmp/jenkinsgit
